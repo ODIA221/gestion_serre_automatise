e1876e67b974815743521bc9e55461e324bbb125
odia221 <odia221@localhost.localdomain>
2023-03-15 10:07:26 +0000

    ok

--- interface/src/App.js
+++ interface/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+import Connexion from './pages/Connexion.js';
 import './App.css';
+import LoginForm from './pages/Connexion.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="pages">
+
+      <LoginForm/>
+
     </div>
   );
 }
