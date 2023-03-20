0d355860b51ffb93a8bbc02e6235fb651a85cac9
odia221 <odia221@localhost.localdomain>
2023-03-20 11:16:52 +0000

    ok

--- interface/src/App.js
+++ interface/src/App.js
@@ -1,6 +1,8 @@
 import {Routes, Route, createSearchParams } from 'react-router-dom';
 import LoginForm from './pages/Connexion.js';
 import Paramettre from './pages/Paramettre.js';
+import Dashboard from './pages/Dashboard';
+import Historiques from './pages/Historiques'
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
       <Routes>
          <Route path="/" element={<LoginForm/>} />
          <Route path="/param" element={<Paramettre/>} />
+         <Route path="/historique" element={<Historiques/>} />
+         <Route path="/dashbord" element={<Dashboard/>} />
 
       </Routes>
 
