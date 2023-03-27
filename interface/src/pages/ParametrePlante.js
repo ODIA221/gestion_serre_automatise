7f7ecaf3a57dd6f17454a299f3ed8f907988933f
aissatou7 <yayefallsaliou@gmail.com>
2023-03-27 16:32:14 +0000

    ok

--- interface/src/pages/ParametrePlante.js
+++ interface/src/pages/ParametrePlante.js
@@ -3,14 +3,53 @@ import React from 'react'
 const ParametrePlante = () => {
   return (
     <div class='historiquePlante2'>
-      <span class='HeaderHistoriquePlante2'>Historique des plantes</span>
-        <div class='divHistoriquePlante2'> 
-        <button class='btnHistoriquePlante2'> Salade</button>
-        <button class='btnHistoriquePlante2'> Tomate</button>
-        <button class='btnHistoriquePlante2'> Persil Chinois</button>
+       <div class='paraPlante'> 
+        <div class='paraNom'><h1>Tomate</h1> </div>
+       
+        <div class='groupChampPara'>
+        <div class='champPara'>
+          <h4>Première heure :</h4>
+          <h5>8H</h5>
         </div>
-
+        <div class='champPara'>
+          <h4>Durée :</h4>
+          <h5>10mns</h5>
+        </div>
+        <div class='champPara'>
+          <h4>Deuxième heure :</h4>
+          <h5>16H</h5>
+        </div>
+        <div class='champPara'>
+          <h4>Durée :</h4>
+          <h5>15mns</h5>
+        </div>
+        </div>
+      </div>
+      <div class='paraPlante'> 
+        <div class='paraNom'><h1>Salade</h1> </div>
+       
+        <div class='groupChampPara'>
+        <div class='champPara'>
+          <h4>Première heure :</h4>
+          <h5>8H</h5>
+        </div>
+        <div class='champPara'>
+          <h4>Durée :</h4>
+          <h5>5mns</h5>
+        </div>
+        <div class='champPara'>
+          <h4>Deuxième heure :</h4>
+          <h5>17H</h5>
+        </div>
+        <div class='champPara'>
+          <h4>Durée :</h4>
+          <h5>5mns</h5>
+        </div>
+        </div>
+      </div>
     </div>
+
+    
   )
 }
 
