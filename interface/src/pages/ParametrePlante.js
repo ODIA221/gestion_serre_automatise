2382e5d97b3cf10219c8d3ce8d04fa3a46d3b2c8
aissatou7 <yayefallsaliou@gmail.com>
2023-03-28 08:45:43 +0000

    ok

--- interface/src/pages/ParametrePlante.js
+++ interface/src/pages/ParametrePlante.js
@@ -4,45 +4,45 @@ const ParametrePlante = () => {
   return (
     <div class='historiquePlante2'>
        <div class='paraPlante'> 
-        <div class='paraNom'><h1>Tomate</h1> </div>
+        <div class='paraNom'><h1><b>Tomate</b></h1> </div>
        
         <div class='groupChampPara'>
         <div class='champPara'>
-          <h4>Première heure :</h4>
+          <h4><b>Première heure :</b></h4>
           <h5>8H</h5>
         </div>
         <div class='champPara'>
-          <h4>Durée :</h4>
+          <h4><b>Durée :</b></h4>
           <h5>10mns</h5>
         </div>
         <div class='champPara'>
-          <h4>Deuxième heure :</h4>
+          <h4><b>Deuxième heure :</b></h4>
           <h5>16H</h5>
         </div>
         <div class='champPara'>
-          <h4>Durée :</h4>
+          <h4><b>Durée :</b></h4>
           <h5>15mns</h5>
         </div>
         </div>
       </div>
       <div class='paraPlante'> 
-        <div class='paraNom'><h1>Salade</h1> </div>
+        <div class='paraNom'><h1><b>Salade</b></h1> </div>
        
         <div class='groupChampPara'>
         <div class='champPara'>
-          <h4>Première heure :</h4>
+          <h4><b>Première heure :</b></h4>
           <h5>8H</h5>
         </div>
         <div class='champPara'>
-          <h4>Durée :</h4>
+          <h4><b>Durée :</b></h4>
           <h5>5mns</h5>
         </div>
         <div class='champPara'>
-          <h4>Deuxième heure :</h4>
+          <h4><b>Deuxième heure :</b></h4>
           <h5>17H</h5>
         </div>
         <div class='champPara'>
-          <h4>Durée :</h4>
+          <h4><b>Durée :</b></h4>
           <h5>5mns</h5>
         </div>
         </div>
