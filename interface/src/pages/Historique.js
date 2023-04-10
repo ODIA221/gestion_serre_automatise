dc0953bf115d3880d8dedc0d8ec73c57e993137c
odia221 <odia221@localhost.localdomain>
2023-04-10 12:53:25 +0000

    ok

--- interface/src/pages/Historique.js
+++ interface/src/pages/Historique.js
@@ -1,168 +1,96 @@
-/* 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Style2.css";
-import { useEffect } from 'react'
-
+import axios from 'axios';
 
 function Historiques() {
-  const [donnee, setData] = useState([])
-  const [start, setStart] = useState(0);
-  const [end, setEnd] = useState(7);
-  const [active1, setActive1] = useState(true);
-  const [active2, setActive2] = useState(false);
-  const [rechercher, setRecherche] = useState("");
-  const [cacher2, setCacher2] = useState(true);
-
+  const [historiqueData, setHistoriqueData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage, setItemsPerPage] = useState(10);
+  const [searchDate, setSearchDate] = useState('');
 
   useEffect(() => {
-    fetch("http://localhost:5000/api/recu", {
-      method: "GET", headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-        "Access-Control-Allow-Origin": "*",
-      },
-    })
-      .then((res) => res.json())
-      .then((res) => {
-        console.log(res);
-        setData(
-          res.filter((_a, index) => {
-            if (rechercher === "") {
-              return index >= start && index < end;
-            } else {
-              if (rechercher != _a.date) {
-                console.log("rien");
-
-              }
-
-            if (rechercher === _a.date) {
-              console.log("trouver");
-              setCacher2(true); 
-              return _a.date === rechercher;
-            }
-          }
-          return false;
-          }
-          )
-        );
-
-
-
+    axios.get("http://localhost:5000/api/recu")
+      .then(response => {
+        setHistoriqueData(response.data);
+      })
+      .catch(error => {
+        console.log(error);
       });
+  }, []);
 
-  }, [start, end, rechercher, cacher2]);
-  const fleche = () => {
-    if (active1 === true) {
-      setActive1(false);
-      setActive2(true);
-      setStart(7);
-      setEnd(14);
-    }
-    if (active2 === true) {
-      setActive1(true);
-      setActive2(false);
-      setStart(0);
-      setEnd(7);
-    }
+  const handleSearch = () => {
+    axios.get(`http://localhost:5000/api/recu?jour=${searchDate}`)
+      .then(response => {
+        setHistoriqueData(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  // Calcul du nombre total de pages
+  const pageCount = Math.ceil(historiqueData.length / itemsPerPage);
+
+  // Fonction pour passer à la page suivante
+  const handleNextPage = () => {
+    setCurrentPage(currentPage + 1);
   };
-  const search = (chercher) => {
-    const valeur = chercher;
-    setRecherche(valeur);
+
+  // Fonction pour passer à la page précédente
+  const handlePrevPage = () => {
+    setCurrentPage(currentPage - 1);
   };
+
+  // Fonction pour changer de page
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  // Index de départ pour l'affichage des données
+  const startIndex = (currentPage - 1) * itemsPerPage;
+
+  // Index de fin pour l'affichage des données
+  const endIndex = startIndex + itemsPerPage;
+
   return (
-    <div class= "container">
-       <input
-              onChange={(e) => search(e.target.value)}
-              type="date"
-              name="date"
-              id="date"
-            />
-    <table class=" table border border-dark mt-4">
-      <thead>
-        <tr>
-          <th>Jours/ Données</th>
-          <th>Température</th>
-          <th>Humidité sol</th>
-          <th>Humidité serre</th>
-          <th>Luminosité</th>
-        </tr>
-      </thead>
-      <tbody>
- 
-         {
-          donnee.map(h => <tr>
-            <td><b>{h.jour}</b></td>
-            <td>{h.temperature}°C</td>
-            <td>{h.humsol}%</td>
-            <td>{h.humserre}%</td>
-            <td>{h.luminosite}lux</td>
-          </tr>)} 
-      </tbody>
-    </table>
-    <div className="box-pagination">
-              <nav aria-label="Page navigation example">
-                <ul className="pagination pagination_ ">
-                  <li className="page-item ">
-                    <a
-                      className={`pagenav ${cacher2 ? "cacher" : ""}`}
-                      href="#"
-                      aria-label="Previous"
-                      onClick={() => {
-                        fleche();
-                      }}
-                    >
-                      <span aria-hidden="true">&laquo;</span>
-                    </a>
-                  </li>
-                  <li className="page-item">
-                    <a
-                      className={`pagelinkupdate ${
-                        active1 ? "bg-focus" : ""
-                      }  `}
-                      id="un"
-                      href="#"
-                      onClick={() => {
-                        setStart(0);
-                        setEnd(7);
-                        setActive1(true);
-                        setActive2(false);
-                      }}
-                    >
-                      1
-                    </a>
-                  </li>
-                  <li className="page-item">
-                    <a
-                      className={`pagelinkupdate ${active2 ? "bg-focus" : ""} ${
-                        cacher2 ? "cacher" : ""
-                      }`}
-                      href="#"
-                      onClick={() => {
-                        setStart(7);
-                        setEnd(11);
-                        setActive1(false);
-                        setActive2(true);
-                      }}
-                    >
-                      2
-                    </a>
-                  </li>
-                  <li className="page-item">
-                    <a
-                      className={`pagenav ${cacher2 ? "cacher" : ""}`}
-                      href="#"
-                      onClick={() => fleche()}
-                      aria-label="Next"
-                    >
-                      <span aria-hidden="true">&raquo;</span>
-                    </a>
-                  </li>
-                </ul>
-              </nav>
-  </div>
-</div>
+    <div className="container">
+      <h1>Historique des données</h1>
+      <div className="search">
+        <input type="text" placeholder="Rechercher par date (AAAA-MM-JJ)" value={searchDate} onChange={(e) => setSearchDate(e.target.value)} />
+        <button onClick={handleSearch}>Rechercher</button>
+      </div>
+      <table>
+        <thead>
+          <tr>
+            <th>Jour</th>
+            <th>Température</th>
+            <th>Humidité sol</th>
+            <th>Humidité serre</th>
+            <th>Luminosité</th>
+          </tr>
+        </thead>
+        <tbody>
+          {historiqueData.slice(startIndex, endIndex).map((data, index) => (
+            <tr key={index}>
+              <td>{data.jour}</td>
+              <td>{data.temperature}</td>
+              <td>{data.humsol}</td>
+              <td>{data.humserre}</td>
+              <td>{data.luminosite}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <div>
+        {currentPage > 1 && (
+          <button onClick={handlePrevPage}>Précédent</button>
+        )}
+        {currentPage < pageCount && (
+          <button onClick={handleNextPage}>Suivant</button>
+        )}
+      </div>
+    </div>
   );
 }
 
-
-export default Historiques; */
+export default Historiques;
--- interface/src/pages/Historiques.js
+++ interface/src/pages/Historiques.js
@@ -1,264 +1,88 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { useState, useEffect } from "react";
 import "./Style2.css";
+import axios from 'axios';
 
-function Historiques() {
-  const [donnee, setData] = useState([]);
-  const [start, setStart] = useState(0);
-  const [end, setEnd] = useState(7);
 
-  const [rechercher, setRecherche] = useState("");
-  const [cacher2, setCacher2] = useState(true);
-
-/*   const [items, setItems] = useState(["..."]);
-  const [activePage, setActivePage] = useState(1);
- */
-
-  const items = [
-    { id: 1, name: "Item 1" },
-    { id: 2, name: "Item 2" },
-    { id: 3, name: "Item 3" },
-    { id: 4, name: "Item 4" },
-    { id: 5, name: "Item 5" },
-    { id: 6, name: "Item 6" },
-    { id: 7, name: "Item 7" },
-    { id: 8, name: "Item 8" },
-    { id: 9, name: "Item 9" },
-    { id: 10, name: "Item 10" },
-    { id: 11, name: "Item 11" },
-    { id: 12, name: "Item 12" },
-    { id: 13, name: "Item 13" },
-    { id: 14, name: "Item 14" },
-    { id: 15, name: "Item 15" },
-    { id: 16, name: "Item 16" },
-    { id: 17, name: "Item 17" },
-    { id: 18, name: "Item 18" },
-    { id: 19, name: "Item 19" },
-    { id: 20, name: "Item 20" }
-  ];
   
-  const Pagination = () => {
-    const [displayedItems, setDisplayedItems] = useState([]);
-    const [activePage, setActivePage] = useState(1);
-    const [active1, setActive1] = useState(true);
-    const [active2, setActive2] = useState(false);
-  
-    const pageSize = 7;
-  
-    const handlePagination = (page) => {
-      // nombre d'éléments par page
-      const startIndex = (page - 1) * pageSize;
-      const endIndex = Math.min(startIndex + pageSize, items.length);
-      // si la fin de la page dépasse la fin du tableau, ajuster la fin
-      const remaining = items.length - endIndex;
-      if (remaining > 0) {
-        // il y a des éléments restants
-        console.log(`Affichage de ${pageSize} éléments de la page ${page}`);
-        const newPageItems = items.slice(startIndex, endIndex);
-        setDisplayedItems(newPageItems);
-        setActivePage(page);
-        setActive1(true);
-        setActive2(false);
-      } else {
-        // pas d'éléments restants
-        console.log(`Affichage de ${pageSize} éléments de la page ${page}`);
-        const newPageItems = items.slice(startIndex, endIndex);
-        setDisplayedItems(newPageItems);
-        setActivePage(page);
-        setActive1(false);
-        setActive2(true);
-      }
-    }
-  };
-  
-  const handlePrevClick = () => {
-    if (activePage > 1) {
-      handlePagination(activePage - 1);
-    }
-  };
+  function Historiques() {
+    const [historiqueData, setHistoriqueData] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [itemsPerPage, setItemsPerPage] = useState(10);
+    const [searchDate, setSearchDate] = useState('');
 
-  const handleNextClick = () => {
-    if (activePage < items.length / pageSize) {
-      handlePagination(activePage + 1);
-    }
-  };
-
-  const paginationItems = [];
-  for (let i = 1; i <= Math.ceil(items.length / pageSize); i++) {
-    paginationItems.push(
-      <button
-        key={i}
-        onClick={() => handlePagination(i)}
-        className={`pagination-item ${activePage === i ? "active" : ""}`}
-      >
-        {i}
-      </button>
-    );
-  }
+  
+    useEffect(() => {
+      axios.get("http://localhost:5000/api/recu")
+        .then(response => {
+          setHistoriqueData(response.data);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }, []);
 
     
-
-
-  useEffect(() => {
-    fetch("http://localhost:5000/api/recu", {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-        "Access-Control-Allow-Origin": "*",
-      },
-    })
-      .then((res) => res.json())
-      .then((res) => {
-        const donneeAvecDate = res.map((item) => ({
-          ...item,
-          dateInsertion: Date.now(),
-        }));
-        setData(donneeAvecDate);
-      });
-  }, []);
-
-  const handleSearch = (event) => {
-    const valeur = event.target.value;
-    setRecherche(valeur);
-  };
-
-/*   const handlePagination = (page) => {
-    if (page === 1) {
-      setStart(0);
-      setEnd(7);
-      setActive1(true);
-      setActive2(false);
-    } else if (page === 2) {
-      setStart(7);
-      setEnd(11);
-      setActive1(false);
-      setActive2(true);
-    }
-  }; */
-
-/*   const handlePagination = (page) => {
-    const pageSize = 7; // nombre d'éléments par page
-    const startIndex = (page - 1) * pageSize;
-    const endIndex = Math.min(startIndex + pageSize, items.length);
-    // si la fin de la page dépasse la fin du tableau, ajuster la fin
-    const remaining = items.length - endIndex;
-    if (remaining > 0) {
-      // il y a des éléments restants
-      console.log(`Affichage de ${pageSize} éléments de la page ${page}`);
-      for (let i = startIndex; i < endIndex; i++) {
-        console.log(items[i]);
-      }
-      console.log(`Il reste ${remaining} éléments`);
-      setActive1(true);
-      setActive2(false);
-    } else {
-      // pas d'éléments restants
-      console.log(`Affichage de ${pageSize} éléments de la page ${page}`);
-      for (let i = startIndex; i < endIndex; i++) {
-        console.log(items[i]);
-      }
-      setActive1(false);
-      setActive2(true);
-    }
-  }; */
-
-/*   const pageSize = 7; */
-
-/*   const handlePagination = (page) => {
-     // nombre d'éléments par page
-    const startIndex = (page - 1) * pageSize;
-    const endIndex = Math.min(startIndex + pageSize, items.length);
-    // si la fin de la page dépasse la fin du tableau, ajuster la fin
-    const remaining = items.length - endIndex;
-    if (remaining > 0) {
-      // il y a des éléments restants
-      console.log(`Affichage de ${pageSize} éléments de la page ${page}`);
-      const newPageItems = items.slice(startIndex, endIndex);
-      setDisplayedItems(newPageItems);
-      setActivePage(page);
-      setActive1(true);
-      setActive2(false);
-    } else {
-      // pas d'éléments restants
-      console.log(`Affichage de ${pageSize} éléments de la page ${page}`);
-      const newPageItems = items.slice(startIndex, endIndex);
-      setDisplayedItems(newPageItems);
-      setActivePage(page);
-      setActive1(false);
-      setActive2(true);
-    }
-  }; */
   
+    // Calcul du nombre total de pages
+    const pageCount = Math.ceil(historiqueData.length / itemsPerPage);
   
+    // Fonction pour passer à la page suivante
+    const handleNextPage = () => {
+      setCurrentPage(currentPage + 1);
+    };
   
-
-  const dataFiltree = donnee.filter((item) => {
-    if (rechercher === "") {
-      return true;
-    } else {
-      const dateSelectionnee = new Date(rechercher);
-      const dateItem = new Date(item.dateInsertion);
-      return (
-        dateSelectionnee.toDateString() === dateItem.toDateString() ||
-        dateSelectionnee.toISOString() === dateItem.toISOString()
-      );
-    }
-  });
-
-  const dataAffichee = dataFiltree.slice(start, end);
-
-  return (
-    <div className="container">
-      <div className="form-group">
-        <label htmlFor="date">Sélectionner une date:</label>
-        <input
-          onChange={handleSearch}
-          type="date"
-          name="date"
-          id="date"
-          min="2023-01-01"
-        />
-      </div>
-      <table className="table border border-dark mt-4">
-        <thead>
-          <tr>
-            <th>Date Ajout</th>
-            <th>Température</th>
-            <th>Humidité sol</th>
-            <th>Humidité serre</th>
-            <th>Luminosité</th>
+    // Fonction pour passer à la page précédente
+    const handlePrevPage = () => {
+      setCurrentPage(currentPage - 1);
+    };
+  
+    // Fonction pour changer de page
+    const handlePageChange = (pageNumber) => {
+      setCurrentPage(pageNumber);
+    };
+  
+    // Index de départ pour l'affichage des données
+    const startIndex = (currentPage - 1) * itemsPerPage;
+  
+    // Index de fin pour l'affichage des données
+    const endIndex = startIndex + itemsPerPage;
+  
+    return (
+      <div>
+        <h1>Historique des données</h1>
+        <table classe="historique">
+          <thead>
+            <tr>
+              <th>Jour</th>
+              <th>Température</th>
+              <th>Humidité sol</th>
+              <th>Humidité serre</th>
+              <th>Luminosité</th>
             </tr>
           </thead>
-            <tbody>
-              {dataAffichee.map((item) => (
-              <tr key={item.id}>
-                <td>{new Date(item.dateInsertion).toLocaleDateString()}</td>
-                <td>{item.temperature}</td>
-                <td>{item.humsol}</td>
-                <td>{item.humserre}</td>
-                <td>{item.luminosite}</td>
+          <tbody>
+            {historiqueData.slice(startIndex, endIndex).map((data, index) => (
+              <tr key={index}>
+                <td>{data.jour}</td>
+                <td>{data.temperature}</td>
+                <td>{data.humsol}</td>
+                <td>{data.humserre}</td>
+                <td>{data.luminosite}</td>
               </tr>
-              ))}
-            </tbody>
-            </table>
-            <nav aria-label="Page navigation example">
-              <ul className="pagination justify-content-center">
-                  
-
-              </ul>
-              
-            </nav>
-
-            </div>
-            );
-              
-            }
-
-export default Historiques;
-
-
-
-
-
-
+            ))}
+          </tbody>
+        </table>
+        <div>
+          {currentPage > 1 && (
+            <button onClick={handlePrevPage}>Précédent</button>
+          )}
+          {currentPage < pageCount && (
+            <button onClick={handleNextPage}>Suivant</button>
+          )}
+        </div>
+      </div>
+    );
+  }
+  
+  export default Historiques;
+  
