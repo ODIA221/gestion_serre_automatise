594edd2f167d44e27b91148ea95319e037af7152
odia221 <odia221@localhost.localdomain>
2023-04-10 05:55:26 +0000

    ok

--- interface/src/pages/Historiques.js
+++ interface/src/pages/Historiques.js
@@ -6,13 +6,98 @@ function Historiques() {
   const [donnee, setData] = useState([]);
   const [start, setStart] = useState(0);
   const [end, setEnd] = useState(7);
-  const [active1, setActive1] = useState(true);
-  const [active2, setActive2] = useState(false);
+
   const [rechercher, setRecherche] = useState("");
   const [cacher2, setCacher2] = useState(true);
 
-  const [items, setItems] = useState(["..."]); // définir les éléments du tableau
+/*   const [items, setItems] = useState(["..."]);
   const [activePage, setActivePage] = useState(1);
+ */
+
+  const items = [
+    { id: 1, name: "Item 1" },
+    { id: 2, name: "Item 2" },
+    { id: 3, name: "Item 3" },
+    { id: 4, name: "Item 4" },
+    { id: 5, name: "Item 5" },
+    { id: 6, name: "Item 6" },
+    { id: 7, name: "Item 7" },
+    { id: 8, name: "Item 8" },
+    { id: 9, name: "Item 9" },
+    { id: 10, name: "Item 10" },
+    { id: 11, name: "Item 11" },
+    { id: 12, name: "Item 12" },
+    { id: 13, name: "Item 13" },
+    { id: 14, name: "Item 14" },
+    { id: 15, name: "Item 15" },
+    { id: 16, name: "Item 16" },
+    { id: 17, name: "Item 17" },
+    { id: 18, name: "Item 18" },
+    { id: 19, name: "Item 19" },
+    { id: 20, name: "Item 20" }
+  ];
+  
+  const Pagination = () => {
+    const [displayedItems, setDisplayedItems] = useState([]);
+    const [activePage, setActivePage] = useState(1);
+    const [active1, setActive1] = useState(true);
+    const [active2, setActive2] = useState(false);
+  
+    const pageSize = 7;
+  
+    const handlePagination = (page) => {
+      // nombre d'éléments par page
+      const startIndex = (page - 1) * pageSize;
+      const endIndex = Math.min(startIndex + pageSize, items.length);
+      // si la fin de la page dépasse la fin du tableau, ajuster la fin
+      const remaining = items.length - endIndex;
+      if (remaining > 0) {
+        // il y a des éléments restants
+        console.log(`Affichage de ${pageSize} éléments de la page ${page}`);
+        const newPageItems = items.slice(startIndex, endIndex);
+        setDisplayedItems(newPageItems);
+        setActivePage(page);
+        setActive1(true);
+        setActive2(false);
+      } else {
+        // pas d'éléments restants
+        console.log(`Affichage de ${pageSize} éléments de la page ${page}`);
+        const newPageItems = items.slice(startIndex, endIndex);
+        setDisplayedItems(newPageItems);
+        setActivePage(page);
+        setActive1(false);
+        setActive2(true);
+      }
+    }
+  };
+  
+  const handlePrevClick = () => {
+    if (activePage > 1) {
+      handlePagination(activePage - 1);
+    }
+  };
+
+  const handleNextClick = () => {
+    if (activePage < items.length / pageSize) {
+      handlePagination(activePage + 1);
+    }
+  };
+
+  const paginationItems = [];
+  for (let i = 1; i <= Math.ceil(items.length / pageSize); i++) {
+    paginationItems.push(
+      <button
+        key={i}
+        onClick={() => handlePagination(i)}
+        className={`pagination-item ${activePage === i ? "active" : ""}`}
+      >
+        {i}
+      </button>
+    );
+  }
+
+    
+
 
   useEffect(() => {
     fetch("http://localhost:5000/api/recu", {
@@ -52,7 +137,7 @@ function Historiques() {
     }
   }; */
 
-  const handlePagination = (page) => {
+/*   const handlePagination = (page) => {
     const pageSize = 7; // nombre d'éléments par page
     const startIndex = (page - 1) * pageSize;
     const endIndex = Math.min(startIndex + pageSize, items.length);
@@ -76,7 +161,35 @@ function Historiques() {
       setActive1(false);
       setActive2(true);
     }
-  };
+  }; */
+
+/*   const pageSize = 7; */
+
+/*   const handlePagination = (page) => {
+     // nombre d'éléments par page
+    const startIndex = (page - 1) * pageSize;
+    const endIndex = Math.min(startIndex + pageSize, items.length);
+    // si la fin de la page dépasse la fin du tableau, ajuster la fin
+    const remaining = items.length - endIndex;
+    if (remaining > 0) {
+      // il y a des éléments restants
+      console.log(`Affichage de ${pageSize} éléments de la page ${page}`);
+      const newPageItems = items.slice(startIndex, endIndex);
+      setDisplayedItems(newPageItems);
+      setActivePage(page);
+      setActive1(true);
+      setActive2(false);
+    } else {
+      // pas d'éléments restants
+      console.log(`Affichage de ${pageSize} éléments de la page ${page}`);
+      const newPageItems = items.slice(startIndex, endIndex);
+      setDisplayedItems(newPageItems);
+      setActivePage(page);
+      setActive1(false);
+      setActive2(true);
+    }
+  }; */
+  
   
   
 
@@ -131,17 +244,10 @@ function Historiques() {
             </table>
             <nav aria-label="Page navigation example">
               <ul className="pagination justify-content-center">
-                <li className={`page-item ${active1 ? "active" : ""}`}>
-                  <a className="page-link" href="#" onClick={() => handlePagination(1, items)}>
-                    Prec
-                  </a>
-                </li>
-                <li className={`page-item ${active2 ? "active" : ""}`}>
-                  <a className="page-link" href="#" onClick={() => handlePagination(2, items)}>
-                    Suiv
-                  </a>
-                </li>
+                  
+
               </ul>
+              
             </nav>
 
             </div>
