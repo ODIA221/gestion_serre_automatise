f3cc68d1086f2d4ec8d0243dfd403276134648e0
odia221 <odia221@localhost.localdomain>
2023-04-10 14:21:19 +0000

    ok

--- interface/src/pages/Historique.js
+++ interface/src/pages/Historique.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+/* import React, { useState, useEffect } from "react";
 import "./Style2.css";
 import axios from 'axios';
 
@@ -85,4 +85,4 @@ import axios from 'axios';
   }
   
   export default Historiques;
-  
+   */
